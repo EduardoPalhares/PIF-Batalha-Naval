3d9c0a691427529a99295644f594c3ddf9e65943
MatheusArantes87 <matheus.arantes2006@gmail.com>
2025-11-13 18:55:30 -0300

    Update fleet.h

---
+++ src/fleet.h
@@ -0,0 +1,21 @@
+#ifndef FLEET_H
+#define FLEET_H
+
+#include "orientation.h"
+
+typedef struct {
+    int row;           // linha inicial
+    int col;           // coluna inicial
+    int size;          // tamanho do navio
+    Orientation orientation; // Vertical ou Horizontal
+    int hits;          // quantas vezes já foi acertado
+} Ship;
+
+#define MAX_SHIPS 5
+
+typedef struct {
+    Ship ships[MAX_SHIPS];  
+    int ship_count;         
+} Fleet;
+
+#endif

fb2a5aa0091b0244d5384189d6a8773bc030555b
MatheusArantes87 <matheus.arantes2006@gmail.com>
2025-11-13 18:27:42 -0300

    Update board.h

---
+++ src/board.h
@@ -0,0 +1,21 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <stdio.h>
+#include "cell_state.h"
+#include "orientation.h"
+
+typedef struct {
+    CellState state;
+    Orientation orientation;
+    int row;
+    int col;
+} Cell;
+
+typedef struct {
+    int rows;
+    int cols;
+    Cell cells[10][10];
+} Board;
+
+#endif

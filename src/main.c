da86e2111c49240ee2c31afcc4ece107f9e662f3
MatheusArantes87 <matheus.arantes2006@gmail.com>
2025-11-13 16:12:45 -0300

    main.c

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../include/cell_state.h"
+#include "../include/orientation.h"
+
+int main() {
+    enum CellState estado = CELL_SHIP;
+    enum Orientation direcao = ORIENTATION_VERTICAL;
+
+    printf("Estado da célula: %d\n", estado);
+    printf("Orientação do navio: %d\n", direcao);
+
+    // Simulando alteração de estado
+    estado = CELL_HIT;
+    printf("Novo estado: %d\n", estado);
+
+    return 0;
+}

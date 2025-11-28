8f495ff66202475f7c3857835cfc4a6697d242ac
MatheusArantes87 <matheus.arantes2006@gmail.com>
2025-11-28 08:15:29 -0300

    Update io.h

--- src/io.h
+++ src/io.h
@@ -3,8 +3,8 @@
 #ifndef IO_H
 #define IO_H
 
+#include "game.h" 
 #include "board.h"
-#include "game.h"
 #include <stdbool.h>
 
 bool io_converter_coord(const char *input, int *row,int *col);               //conversão de string para número
@@ -17,8 +17,8 @@ void io_get_player_names(char *p1_nickname, char *p2_nickname);             // S
 
 bool io_get_shot_coord(int max_size, int *row, int *col);                // Lê e valida a coordenada de tiro, garantindo que esteja dentro dos limites.
 
-void io_show_shot_result(CellState result, const char *ship_name, bool is_sunk);               // Exibe o resultado de um tiro com base no estado da célula e no status do navio.
+void io_show_shot_result(CellState result, const char *ship_name, bool is_sunk);              // Exibe o resultado de um tiro com base no estado da célula e no status do navio.
 
-void io_show_winner_stats(const Player *winner, const Player *loser);                     // Exibe o vencedor e as estatísticas de ambos os jogadores ao final do jogo.
+void io_show_winner_stats(const Player *winner, const Player *loser);                 // Exibe o vencedor e as estatísticas de ambos os jogadores ao final do jogo.
 
 #endif
